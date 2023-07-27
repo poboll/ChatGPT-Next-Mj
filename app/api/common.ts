b6f40022d69df8ba3dd18daca912816d350da417
在虎 <caiths@icloud.com>
2023-07-27 22:49:29 +0800

    🔨更新接口

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 
-export const OPENAI_URL = "chimeragpt.adventblocks.cc";
+export const OPENAI_URL = "chimeragpt.adventblocks.cc/api";
 const DEFAULT_PROTOCOL = "https";
 const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
